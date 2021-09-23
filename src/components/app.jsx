6caea221807eee58fdd1a63f134697954e0967a7
aaron staes <aaron.staes@gmail.com>
2021-09-23 10:34:39 +0200

    created Component files and basic layout

---
+++ src/components/app.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from "react";
+
+import FlatList from "../containers/flat_list";
+import Map from "../containers/map";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="row">
+        <FlatList />
+        <Map />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/map.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from "react";
+
+class Map extends Component {
+  render() {
+    const style = {
+      height: "100vh",
+    };
+    return (
+      <div className="col-sm-5" style={style}>
+        TODO
+      </div>
+    );
+  }
+}
+
+export default Map;
